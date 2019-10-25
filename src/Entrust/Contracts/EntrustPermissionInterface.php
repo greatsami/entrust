4d317d508d2b4896ddc1b41d67314f9afb82e314
Sami Mansour <samiman.net@gmail.com>
2019-10-25 17:11:46 +0300

    First Init

---
+++ src/Entrust/Contracts/EntrustPermissionInterface.php
@@ -0,0 +1,19 @@
+<?php
+namespace Greatsami\Entrust\Contracts;
+
+/**
+ * This file is part of Entrust,
+ * a role & permission management solution for Laravel.
+ *
+ * @license MIT
+ * @package Greatsami\Entrust
+ */
+interface EntrustPermissionInterface
+{
+    /**
+     * Many-to-Many relations with role model.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function roles();
+}
---
+++ src/views/generators/migration.blade.php
@@ -0,0 +1,91 @@
+<?php echo '<?php' ?>
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class EntrustSetupTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::beginTransaction();
+
+        // Create table for storing roles
+        Schema::create('{{ $rolesTable }}', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->unique();
+            $table->string('display_name')->nullable();
+            $table->string('description')->nullable();
+            $table->string('allowed_route')->nullable();
+            $table->timestamps();
+        });
+
+        // Create table for associating roles to users (Many-to-Many)
+        Schema::create('{{ $roleUserTable }}', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id')->unsigned();
+            $table->unsignedBigInteger('role_id')->unsigned();
+
+            $table->foreign('user_id')->references('{{ $userKeyName }}')->on('{{ $usersTable }}')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('{{ $rolesTable }}')->onUpdate('cascade')->onDelete('cascade');
+
+            $table->primary(['user_id', 'role_id']);
+        });
+
+        // Create table for storing permissions
+        Schema::create('{{ $permissionsTable }}', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->unique();
+            $table->string('display_name')->nullable();
+            $table->string('display_name_ar')->nullable();
+            $table->string('description')->nullable();
+            $table->string('route')->nullable();
+            $table->string('module')->nullable();
+            $table->string('as')->nullable();
+            $table->string('icon')->nullable();
+            $table->integer('parent')->default(0);
+            $table->integer('parent_original')->default(0);
+            $table->integer('appear')->default(0);
+            $table->integer('ordering')->default(0);
+            $table->timestamps();
+        });
+
+        // Create table for associating permissions to roles (Many-to-Many)
+        Schema::create('{{ $permissionRoleTable }}', function (Blueprint $table) {
+            $table->unsignedBigInteger('permission_id');
+            $table->unsignedBigInteger('role_id');
+
+            $table->foreign('permission_id')->references('id')->on('{{ $permissionsTable }}')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('{{ $rolesTable }}')->onUpdate('cascade')->onDelete('cascade');
+
+            $table->primary(['permission_id', 'role_id']);
+        });
+
+        Schema::create('{{ $userPermissionTable }}', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('{{ $usersTable }}')->onUpdate('cascade')->onDelete('cascade');
+            $table->unsignedBigInteger('permission_id');
+            $table->foreign('permission_id')->references('id')->on('{{ $permissionsTable }}')->onUpdate('cascade')->onDelete('cascade');
+            $table->primary(['user_id', 'permission_id']);
+        });
+
+        DB::commit();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('{{ $userPermissionTable }}');
+        Schema::drop('{{ $permissionRoleTable }}');
+        Schema::drop('{{ $permissionsTable }}');
+        Schema::drop('{{ $roleUserTable }}');
+        Schema::drop('{{ $rolesTable }}');
+    }
+}
